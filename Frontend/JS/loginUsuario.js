291963b837e5c622d5cd538753b4d4f6d4c1037b
moru <lospurtes@gmail.com>
2022-06-05 21:15:16 -0500

    añadido funciones modal

--- Frontend/JS/loginUsuario.js
+++ Frontend/JS/loginUsuario.js
@@ -2,6 +2,8 @@ const usernarmeElement = document.querySelector("#registroEmail");
 const passwordElement = document.querySelector("#registrousername");
 const botonLogin = document.querySelector("#login");
 
+var modal = document.getElementById("popup-box");
+
 const loginHandler = (e) => {
   e.preventDefault();
   const credentials = {
@@ -12,3 +14,33 @@ const loginHandler = (e) => {
   // AQUI VIENE TODA LA LOGICA QUE SE HACE YA CUANDO SE TENGA UNA CONEXION AL BACKEND
 };
 botonLogin.addEventListener("click", loginHandler);
+
+
+
+
+
+//Modal registro
+
+document.addEventListener("click",
+function(event){
+  
+  if(event.target == modal){
+    closeModal();
+  }
+},
+false
+)
+
+function showModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function span() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+function closeModal(event) {
+  document.querySelector(".modal").style.display = "none"
+}
